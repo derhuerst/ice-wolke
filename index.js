a307688aad2d94fbe341207aa2a15139649bc072
Jannis R <mail@jannisr.de>
2016-06-18 04:12:32 +0200

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 'use strict'
 
-// todo
+const ndjson = require('ndjson')
+const express = require('express')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+
+
+
+const db = ndjson.stringify()
+db.pipe(fs.createWriteStream('data.ndjson', {flags: 'a'}))
+
+
+
+const wolke = express()
+wolke.use(bodyParser.urlencoded({extended: true}))
+
+wolke.get('/', (req, res) => {
+	res.status(200)
+	fs.createReadStream('data.ndjson')
+	.pipe(res)
+})
+
+wolke.post('/', (req, res) => {
+	const data = {
+		  when:      +req.body.when
+		, latitude:  +req.body.latitude
+		, longitude: +req.body.longitude
+		, line:       req.body.line
+	}
+	db.write(data)
+	res.status(200).end()
+})
+
+
+
+wolke.listen(3001)
